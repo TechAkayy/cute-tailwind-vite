fc7122bf3e76f74098bdf5b3884a320866864d4f
Akayy <techakayy@gmail.com>
2023-12-09 22:26:54 +1100

    Added mpa support in vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -2,68 +2,28 @@
 
 import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
-
-// import { visualizer } from 'rollup-plugin-visualizer'
+import {liveDesigner} from '@pinegrow/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    //...
-  ],
-  mode: 'development',
-  // base: '/dist',
-  define: {
-    'process.env': process.env,
-  },
-  server: {
-    // open: '/index.html',
-    // port: '5174',
-    // hmr: false
-  },
-  build: {
-    // minify: 'esbuild', // ['esbuild' (default), 'terser', true, false]
-    cssMinify: false, // ['esbuild' (default is same as minify), 'lightningcss', true, false]
-    // sourcemap: false, // [false (default), true]
-    outDir: 'dist',
-    // IMPORTANT: Set to false; otherwise, Pinegrow watchers will cease tracking changes to the individual CSS outputs, preventing automatic style updates to the HTML pages.
-    emptyOutDir: false, // [true (default), false]
-    // https://vitejs.dev/config/build-options.html#build-lib
-    lib: {
-      // entry file that has es modules & the tailwind.css that contains the tailwind directives
-      entry: './src/main.js',
-      // // Can specify multiple entries. Note: When multiple entries are used, remove 'umd' and 'iife' from output formats below
-      // entry: {
-      // 	main: './src/main.js',
-      // 	another: './src/another.js'
-      // },
-      name: 'MyLib',
-      // fileName: '[name]',
-      // fileName: (format, entryAlias) => `${entryAlias}.${format}.js`,
-      fileName: (format, entryAlias) => `${entryAlias}.js`,
-      formats: ['iife'], // can be ['es', 'cjs', 'umd', 'iife'], // defaults to ['es', 'umd'], and ['es', 'cjs'] when multiple entries are used.
-    },
-
-    // Vite uses Rollup under the hold, so rollup options & plugins can be used for advanced usage
-    rollupOptions: {
-      plugins: [
-        /*visualizer()*/
-      ],
-      output: {
-        // Just a simple function to get the css file generated as output.css under dist/assets/css folder. And all the images under dist/assets/img folder.
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1)
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img'
-          }
-          if (extType === 'css') {
-            return `assets/${extType}/output[extname]`
-          } else if (extType === 'img') {
-            return `assets/${extType}/[name]-[hash][extname]`
-          }
-        },
+    liveDesigner({
+      dirs: {
+        src: './src',
       },
-    },
-  },
+      pagesWithEntries: [
+        {
+          page: './index.html',
+          entry: './src/main.js',
+        },
+        {
+          page: './blog.html',
+          entry: './src/main.js',
+        },
+      ],
+    }),
+  ],
+
   resolve: {
     alias: {
       /* Must be either an object, or an array of { find, replacement, customResolver } pairs. */
